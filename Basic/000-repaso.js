94060160675605833d006dbf7851234cefc26f58
Randy Daniel <randyruiz937@gmail.com>
2026-04-21 17:08:07 -0600

    Repaso de variables, tipos de datos y operadores

---
+++ Basic/000-repaso.js
@@ -0,0 +1,212 @@
+// 1. Escribe un comentario en una línea
+//practica
+
+// 2. Escribe un comentario en varias líneas
+/*practica de comentario
+en varias lineas*/
+
+/*
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+let myname = "Randy"
+let age = 22
+let height = 1.78
+let Student = true
+let undefinedValue 
+let nullValue = null
+let mysymbol = Symbol()
+let myBigInt = BigInt(98732471387928732789327984789342)
+
+// 4. Imprime por consola el valor de todas las variables
+console.log(myname);
+console.log(age);
+console.log(height);
+console.log(Student);
+console.log(undefinedValue);
+console.log(nullValue);
+console.log(mysymbol);
+console.log(myBigInt);
+
+// 5. Imprime por consola el tipo de todas las variables
+console.log(typeof myname);
+console.log(typeof age);
+console.log(typeof height);
+console.log(typeof Student);
+console.log(typeof undefinedValue);
+console.log(typeof nullValue);
+console.log(typeof mysymbol);
+console.log(typeof myBigInt);
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+myname = "Daniel"
+age = 27
+height = 1.80
+Student = false
+undefinedValue = undefined
+nullValue = null
+mysymbol = Symbol("newSymbol")
+myBigInt = BigInt(7773293727373279327732732)
+console.log(myname);
+console.log(age);
+console.log(height);
+console.log(Student);
+console.log(undefinedValue);
+console.log(nullValue);
+console.log(mysymbol);
+console.log(myBigInt);
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+myname = 80
+age = "veinte"
+height = "unopuntoocho"
+Student = null
+undefinedValue = "hola"
+nullValue = undefined
+mysymbol = BigInt(938938923829832823892389)
+myBigInt = Symbol("symbol")
+console.log(myname);
+console.log(age);
+console.log(height);
+console.log(Student);
+console.log(undefinedValue);
+console.log(nullValue);
+console.log(mysymbol);
+console.log(myBigInt);
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+const newName = "Randyy"
+const newAge = 22
+const newHeight = 1.78
+const newStudent = true
+const newUndefinedValue = undefined
+const newNullValue = null
+const newMySymbol = Symbol("newSymbol")
+const newMyBigInt = BigInt(1111111111111111111111111111)
+console.log(newName);
+console.log(newAge);
+console.log(newHeight);
+console.log(newStudent);
+console.log(newUndefinedValue);
+console.log(newNullValue);
+console.log(newMySymbol);
+console.log(newMyBigInt);
+
+// 9. A continuación, modifica los valores de las constantes
+newName = "hola"
+newAge = 28
+newHeight = 1.90
+newStudent = false
+newUndefinedValue = undefined
+newNullValue = null
+newMySymbol = Symbol("holaaa")
+newMyBigInt = bigInt(222222222222222222222222222222)
+console.log(newName);
+console.log(newAge);
+console.log(newHeight);
+console.log(newStudent);
+console.log(newUndefinedValue);
+console.log(newNullValue);
+console.log(newMySymbol);
+console.log(newMyBigInt);
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
+
+//TypeError: Assignment to constant variable.
+*/
+
+//--------------------------------------------------------------------------------------
+/*
+// 1. Crea una variable para cada operación aritmética
+let suma = 10 + 5
+let resta = 10 - 5
+let multiplicacion = 10 * 5
+let division = 10 / 5
+let modulo = 10 % 5 
+let exponente = 10 ** 5
+let incremento = 10 
+incremento++;
+let decremento = 10
+decremento--;
+console.log(suma)
+console.log(resta)
+console.log(multiplicacion)
+console.log(division)
+console.log(modulo)
+console.log(exponente)
+console.log(incremento)
+console.log(decremento)
+
+// 2. Crea una variable para cada tipo de operación de asignación,
+//    que haga uso de las variables utilizadas para las operaciones aritméticas
+let suma2 = 15
+suma2 += 15
+let resta2 = 15
+resta2 -= 15
+let multiplicacion2 = 15
+multiplicacion2 *= 15
+let division2 = 15
+division2 /= 15
+let modulo2 = 100
+modulo2 %= 100
+let exponente2 = 15
+exponente2 **= 15
+console.log(suma2)
+console.log(resta2)
+console.log(multiplicacion2)
+console.log(division2)
+console.log(modulo2)
+console.log(exponente2)
+// 3. Imprime 5 comparaciones verdaderas con diferentes operadores de comparación
+console.log(10 > 5)
+console.log(10 == 10)
+console.log(99 < 100)
+console.log(10 <= 10)
+console.log(10 >= 6)
+// 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
+console.log(10 < 5)
+console.log(10 == 9)
+console.log(99 > 100)
+console.log(10 >= 80)
+console.log(10 <= 6)
+// 5. Utiliza el operador lógico and
+console.log(10 < 11 && 90 > 10)
+console.log(10 < 11 && 90 > 10 && 17 >= 17)
+console.log(10 < 11 && 90 < 10)   //con un solo false lo toma como false
+// 6. Utiliza el operador lógico or
+console.log(10 == 10 || 10 < 11)
+console.log(10 == 10 || 10 < 11 || 2 > 1)
+console.log(10 > 20 || 10 > 11)   //tiene que ser todos falsos para que detecte false
+// 7. Combina ambos operadores lógicos
+console.log(15 >= 20 && 12 < 20 || 20 <= 30)
+console.log(15 >= 20 && 12 < 20 || 20 >= 30)
+// 8. Añade alguna negación
+console.log(!(14 < 20))
+console.log(!(14 > 20))
+// 9. Utiliza el operador ternario
+const isStudent = true
+isStudent ? console.log("Es estudiante") : console.log("No es estudiante");
+
+const isTeacher = false
+isTeacher ? console.log("Es profesor") : console.log("no es profesor"); 
+*/
+//-------------------------------------------------------------------------------------
+
+// 1. Concatena dos cadenas de texto
+
+// 2. Muestra la longitud de una cadena de texto
+
+// 3. Muestra el primer y último carácter de un string
+
+// 4. Convierte a mayúsculas y minúsculas un string
+
+// 5. Crea una cadena de texto en varias líneas
+
+// 6. Interpola el valor de una variable en un string
+
+// 7. Reemplaza todos los espacios en blanco de un string por guiones
+
+// 8. Comprueba si una cadena de texto contiene una palabra concreta
+
+// 9. Comprueba si dos strings son iguales
+
+// 10. Comprueba si dos strings tienen la misma longitud
+
+//-------------------------------------------------------------------------------------

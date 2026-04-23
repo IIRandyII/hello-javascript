f673a6445535e348c691b675a57afbaa91bdaf51
Randy Daniel <randyruiz937@gmail.com>
2026-04-22 18:16:13 -0600

    Repaso de arrays, set y map

--- Basic/000-repaso.js
+++ Basic/000-repaso.js
@@ -324,3 +324,53 @@ switch (idioma) {
 */
 
 //-------------------------------------------------------------------------------------------------
+
+// 1. Crea un array que almacene cinco animales
+let myArray = ["Perro", "Gato", "Tigre", "Oso", "Pajaro"]
+console.log(myArray);
+
+// 2. Añade dos más. Uno al principio y otro al final
+myArray.unshift("Conejo")
+myArray.push("Elefante")
+console.log(myArray);
+// 3. Elimina el que se encuentra en tercera posición
+myArray.splice(2, 1)
+console.log(myArray);
+// 4. Crea un set que almacene cinco libros
+let mySet = new Set (["libro1", "libro2", "libro3", "libro4", "libro5"])
+console.log(mySet);
+// 5. Añade dos más. Uno de ellos repetido
+mySet.add("libro6", "libro1")
+console.log(mySet);
+// 6. Elimina uno concreto a tu elección
+mySet.delete("libro4")
+console.log(mySet);
+// 7. Crea un mapa que asocie el número del mes a su nombre
+let myMap = new Map ([
+    [1, "enero"],
+    [2, "febrero"],
+    [3, "marzo"],
+    [4, "abril"],
+    [5, "mayo"],
+    [6, "junio"],
+    [7, "julio"],
+    [8, "agosto"],
+    [9, "septiembre"],
+    [10, "octubre"],
+    [11, "noviembre"],
+    [12, "diciembre"]
+])
+console.log(myMap);
+// 8. Comprueba si el mes número 5 existe en el map e imprime su valor
+if (myMap.has(5)) {
+    console.log(myMap.get(5));
+}
+// 9. Añade al mapa una clave con un array que almacene los meses de verano
+myMap.set("verano", ["junio", "julio", "agosto"]);
+console.log(myMap)
+// 10. Crea un Array, transfórmalo a un Set y almacénalo en un Map
+let myNewArray = ["Randy", "Daniel"]
+let myNewSet = new Set (myNewArray);
+let myNewMap = new Map ();
+myNewMap.set("Nombre", myNewSet);
+console.log(myNewMap);
